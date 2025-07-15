62ea22aae193d5dfe5b94c9549de93bc5095597e
Rekha Narang <rekhanarang@Rekhas-MacBook-Air.local>
2025-07-16 01:33:08 +0530

    [Fixed] Otp path

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import PhoneLogin from "./PhoneLogin";
+
 import OTPVerify from "./OtpVerification";
 
 export default function LoginPage({ onSuccess }) {
